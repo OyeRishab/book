feb1fcc91e7be61d1d40f534b58c44e9bb80dc2c
Rishab Jain <85238177+OyeRishab@users.noreply.github.com>
2024-10-03 01:08:04 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,15 +9,15 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
-app.use(express.json()); // Parses JSON request bodies
+app.use(express.json());
 
-// Routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.use("/api/books", bookRoutes);
 app.use("/api/reviews", reviewRoutes);
 
-// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -26,13 +26,15 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
+  setInterval(async () => {
+    await fetch("https://book-a8hg.onrender.com");
+    console.log("Pinged the server");
+  }, 60000 * 4);
 });
